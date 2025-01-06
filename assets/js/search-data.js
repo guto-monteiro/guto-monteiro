1d1814953ff8f520fbf76710494ab2e64f5cdba1
guto-monteiro <eng.agro.gmonteiro@gmail.com>
2025-01-06 03:30:47 +0000

    Deploying to gh-pages from @ guto-monteiro/guto-monteiro@ade3ae5bab3943199e273ebf32932365f2467bbc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -124,16 +124,16 @@ ninja.data = [{
           window.location.href = "/blog/2023/blog-post-1/";
         
       },
-    },{id: "news-researchers-from-the-incr-mrfor-help-to-trace-illegal-wood",
-          title: 'Researchers from the INCR MRfor help to trace illegal wood',
-          description: "",
-          section: "News",},{id: "news-end-of-research-stay-in-germany",
+    },{id: "news-end-of-research-stay-in-germany",
           title: 'End of research stay in Germany',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-see-out-new-article",
           title: 'See out new article!',
+          description: "",
+          section: "News",},{id: "news-researchers-from-the-incr-mrfor-help-to-trace-illegal-wood",
+          title: 'Researchers from the INCR MRfor help to trace illegal wood',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
